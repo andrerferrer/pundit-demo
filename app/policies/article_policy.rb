f49a30c741118650d92d1f5b0abb03281912a777
Andre Ferrer <andrerferrer@hotmail.com>
2020-05-30 15:42:30 -0300

    Add policies for article

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -4,4 +4,25 @@ class ArticlePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    # anyone can show
+    true
+  end
+
+  def create?
+    # anyone can create
+    true
+  end
+
+  def update?
+    # you can only update if you are the owner of the article
+    @record.user == user
+  end
+
+  def destroy?
+    # Same logic here
+    update?
+  end
+
 end
